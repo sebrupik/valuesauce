7456a4e58b09bcd7953ebf009f65f20db8cc7366
Seb Rupik <sebrupik@gmail.com>
2017-03-23 23:31:39 +0000

    it's a start

---
+++ valuesauce/src/valuesauce/Valuesauce.java
@@ -0,0 +1,22 @@
+/*
+* Revices tunnels from sleetlocust agents. Agreagates SNMP get requests for SNMP targets, 
+* determining the minimum interval, makes the GET requests itself and cache the result.
+* Subsequent GET resquests from sleetlocust agents are served the most recent cahed result
+* for that specific target.
+*/
+package valuesauce;
+
+/**
+ *
+ * @author snr
+ */
+public class Valuesauce {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
